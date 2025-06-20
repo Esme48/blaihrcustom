08fb262988618fcc613a00929c2b0b908225f0f8
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-06-20 13:46:37 -0700

    Added extra data types to the Component Model

--- models/Component.js
+++ models/Component.js
@@ -11,6 +11,21 @@ const ComponentSchema = new mongoose.Schema({
         required: [true, 'Please Provide The Color Of Your Item'],
         maxlength: 100
     },
+    price:{
+        type: Number,
+        required: [true, 'Please Provide The Price Of Your Item'],
+        min: 0
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Please Provide The Quantity Of Your Item'],
+        min: 1
+    },
+    date: {
+        type: Date,
+        required: [true, 'Please Provide The Date Of The Purchase'],
+        default: Date.now
+    },
     status:{
         type: String,
         enum: ['Item Pending Review', 'Item Shipped', 'Item Delivered'],
